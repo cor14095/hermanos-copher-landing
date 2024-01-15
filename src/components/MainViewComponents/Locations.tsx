05e6e3eaf6aee0bd09bd41a4eaacd7b258cece36
Alejandro Cortes <ale.crts1996@gmail.com>
2024-01-14 18:24:16 -0800

    Fix stylings and locations layout.

--- src/components/MainViewComponents/Locations.tsx
+++ src/components/MainViewComponents/Locations.tsx
@@ -1,17 +1,70 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
+import { Button } from '../ReUsableComponents/Button';
 
 export const Locations = () => {
+  let map: google.maps.Map;
+  const center: google.maps.LatLngLiteral = { lat: 14.6472287, lng: -90.5254478 };
+
+  //Markers
+  const markerPosition = [
+    { 'postition': { lat: 14.6472287, lng: -90.5254478 }, 'label': 'Periférico' },
+    { 'postition': { lat: 14.6195206, lng: -90.5468998 }, 'label': 'Calz. San Juan' },
+    { 'postition': { lat: 14.6672277, lng: -90.8494999 }, 'label': 'Chimaltenango' },
+    { 'postition': { lat: 14.3117942, lng: -90.3750463 }, 'label': 'Barberena' },
+    { 'postition': { lat: 14.2990816, lng: -90.7841798 }, 'label': 'Escuintla' },
+    { 'postition': { lat: 14.8405614, lng: -91.5015162 }, 'label': 'Xela' },
+    { 'postition': { lat: 14.9925009, lng: -89.7201585 }, 'label': 'Teculután' },
+    { 'postition': { lat: 15.3010977, lng: -91.4963686 }, 'label': 'Huehuetenango' },
+    { 'postition': { lat: 14.6469972, lng: -90.5257205 }, 'label': 'Centro de Servicio' }
+  ];
+
+  function initMap(): void {
+    map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+      center,
+      zoom: 13
+    });
+
+    // The marker, positioned at HC locations
+    markerPosition.map((mark, index) => {
+      const label = mark.label
+      const position = mark.postition
+
+      const marker = new google.maps.Marker({
+        map: map,
+        position: position,
+        title: label
+      })
+    })
+  }
+
+  useEffect(() => {
+    initMap()
+  })
+
   return (
-    <div>
-      <h1 className='text-hc-blue'>Ubicaciones</h1>
-      <div className='flex flex-row w-screen h-[30vh]'>
-        <div className='bg-hc-blue text-slate-100 w-full text-center flex flex-col items-center '>
-          <h2 className='font-bold text-2xl'>Encuentra la tienda mas cercana</h2>
-          <p>contamos con una amplia variedad de repuestos para todas las marcas y modelos de autos</p>
+    <div className='flex flex-col'>
+      <div className='flex flex-row pl-20 pr-20 pt-10 pb-10 gap-3'>
+        <div className='m-auto text-hc-blue'>
+          <span>Ubicaciones</span>
         </div>
-        <div className='bg-hc-red w-full'>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.153462145149!2d-90.52545318892481!3d14.647228675873478!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a202d931dcd9%3A0x4418b907b1b8af6b!2sHermanos%20Copher%20Perif%C3%A9rico!5e0!3m2!1ses-419!2sgt!4v1705270428752!5m2!1ses-419!2sgt"
-        width="600" height="450" style="${border:0;}" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <div className='h-1 grow m-auto bg-hc-red rounded-md' />
+      </div>
+      <div className='flex flex-row w-screen h-[40vh]'>
+        <div className='bg-hc-blue text-slate-100 w-full flex flex-col'>
+          <div className='flex flex-col mt-auto'>
+            <span className='font-bold text-2xl m-auto'>Encuentra la tienda mas cercana</span>
+            <span className='m-auto'>contamos con una amplia variedad de repuestos para todas las marcas y modelos de autos</span>
+          </div>
+          <div className='flex mb-auto p-10'>
+            <Button
+              text='Tiendas'
+              className='bg-hc-red border-2 border-white m-auto'
+            />
+          </div>
+        </div>
+        <div className='bg-hc-red w-full h-full'>
+          <div id="map" className='w-full h-full'></div>
         </div>
       </div>
     </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,6 +3,9 @@
 import Link from "next/link";
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import { FaWhatsapp } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+
 
 export const NavBar = () => {
   const [nav, setNav] = useState(false)
@@ -10,33 +13,53 @@ export const NavBar = () => {
   const handleNav = () => {
     setNav(!nav)
   }
-  
+
   return (
-    <div id='nav' className='fixed left-0 top-0 w-full bg-hc-red z-20'>
-      <div className='m-auto justify-center items-center p-4 text-inverted-space hidden md:flex'>
-        {/** Web Nav */}
-        <ul className='flex z-20 text-white font-bold'>
-          <li className='p-4 hover:text-black'>
-            <Link href='/#'>Inicio</Link>
-          </li>
-          <li className='p-4 hover:text-black'>
-            <Link href='/#location'>Ubicación</Link>
-          </li>
-          <li className='p-4 hover:text-black'>
-            <Link href='/#contact'>Contacto</Link>
-          </li>
-        </ul>
+    <div id='nav' className='fixed left-0 top-0 w-full bg-hc-red z-20 pl-10 pr-10'>
+      <div className="hidden md:flex">
+        <div className="flex mr-auto">
+          <div className="h-full flex">
+            <span className="m-auto">LogoImg</span>
+          </div>
+        </div>
+        <div className='m-auto justify-center items-center p-4 text-inverted-space flex'>
+          {/** Web Nav */}
+          <ul className='flex z-20 text-white font-bold'>
+            <li className='p-4 hover:text-black'>
+              <Link href='/#'>Inicio</Link>
+            </li>
+            <li className='p-4 hover:text-black'>
+              <Link href='/#location'>Ubicación</Link>
+            </li>
+            <li className='p-4 hover:text-black'>
+              <Link href='/#contact'>Contacto</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex ml-auto flex-col">
+          <div className="flex grow">
+            <a href="tel:22079999" className="mt-auto" target="_blank">
+              <div className="flex flex-row gap-2">
+                <FaPhoneAlt size={20} color="white" className="m-auto" /> <span className="text-white text-xl font-bold m-auto">2207-9999</span>
+              </div>
+            </a>
+          </div>
+          <div className="flex grow">
+            <a href="https://wa.me/+50255643166" className="mb-auto" target="_blank">
+              <div className="flex flex-row mb-auto gap-2">
+                <FaWhatsapp size={20} color="green" className="m-auto" /> <span className="text-white text-xl font-bold m-auto">5564-3166</span>
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
-      <div className="text-slate-100 row-end-1">
-        <h1>2207-9999</h1>
-        <h1>5564-3166</h1>
-      </div>
+
       <div className='m-auto flex justify-end items-center p-4 text-inverted-space md:hidden'>
         {/** Mobile Nav */}
         <div className='block z-10' onClick={handleNav}>
           {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         </div>
-        <div className={`absolute md:hidden top-0 ${ nav ? 'left-0' : 'left-full' } flex justify-center items-center text-center w-full h-screen bg-hc-red/80 ease-in duration-300`}>
+        <div className={`absolute md:hidden top-0 ${nav ? 'left-0' : 'left-full'} flex justify-center items-center text-center w-full h-screen bg-hc-red/80 ease-in duration-300`}>
           <ul>
             <li className='p-5 text-4xl hover:text-black' onClick={handleNav}>
               <Link href='/#'>Inicio</Link>
