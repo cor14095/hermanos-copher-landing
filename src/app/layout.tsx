2199c437ab8292303f7b430a1ceea3389a86b10c
Alejandro Cortes <ale.crts1996@gmail.com>
2024-02-06 13:17:33 -0800

    Fix title and descp to be sinced in with master.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
-  title: 'Copher',
-  description: 'Hermanos Copher, repuestos y piezas.',
+  title: 'Hermanos Copher',
+  description: 'Repuestos Para Todos los Autos — Visita Nuestra Web Y Descubre Todos Los Productos Y Servicios De Hermanos Copher. Reparación De Vehículos Con Garantía Y Calidad. Bumper, Silvin. Amortiguador, Stop.',
 }
 
 const montserrat = Montserrat({ subsets: ['latin'] })
